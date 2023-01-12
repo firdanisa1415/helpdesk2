da5d202142dd1766e0b10a4152c32daabddbbd15
yeremiadio <raikkonendio@gmail.com>
2023-01-12 11:13:03 +0700

    add teleport inside modal detail component

--- src/components/Modal/ModalDetail.vue
+++ src/components/Modal/ModalDetail.vue
@@ -1,3 +1,27 @@
+<template>
+  <Teleport to="body">
+    <Transition name="modal">
+      <div v-if="show" class="modal-mask">
+        <div class="modal-wrapper">
+          <div class="modal-container">
+            <div class="modal-header">
+              <slot name="header">default header</slot>
+            </div>
+
+            <div class="modal-body">
+              <slot name="body">default body</slot>
+            </div>
+
+            <div class="modal-footer">
+              <slot name="footer"> </slot>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Transition>
+  </Teleport>
+</template>
+
 <script>
 export default {
   props: {
@@ -5,31 +29,7 @@ export default {
   },
 };
 </script>
-
-<template>
-  <Transition name="modal">
-    <div v-if="show" class="modal-mask">
-      <div class="modal-wrapper" >
-        <div class="modal-container">
-          <div class="modal-header">
-            <slot name="header">default header</slot>
-          </div>
-
-          <div class="modal-body">
-            <slot name="body">default body</slot>
-          </div>
-
-          <div class="modal-footer">
-            <slot name="footer">
-            </slot>
-          </div>
-        </div>
-      </div>
-    </div>
-  </Transition>
-</template>
-
-<style>
+<style scoped>
 .modal-mask {
   position: fixed;
   z-index: 9998;
