0b8c7e7a8b8b7e5c91aeeea370b74de26384ba49
yeremiadio <raikkonendio@gmail.com>
2023-02-14 12:01:56 +0700

    add userModules

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,9 +1,11 @@
 import { createStore } from "vuex";
 import reportModules from "./modules/reportModules";
+import userModules from "./modules/userModules";
 
 const store = createStore({
   modules: {
     report: reportModules,
+    user: userModules,
   },
 });
 
