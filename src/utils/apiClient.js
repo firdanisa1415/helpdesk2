54488d2936b32e402e38af4d0d70c46fe7fbce96
yeremiadio <raikkonendio@gmail.com>
2023-02-14 12:00:55 +0700

    add axios interceptor

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -1,13 +1,26 @@
 import axios from "axios";
+import CookieHandler from "./cookieHandler";
+const cookieHandler = new CookieHandler();
 
 export default function apiClient() {
-    const apiClient = axios.create({
-        baseURL: process.env.VUE_APP_API_URL,
-        withCredentials:true,
-    });
+  const apiClient = axios.create({
+    baseURL: process.env.VUE_APP_API_URL,
+    withCredentials: true,
+  });
+  /**
+   * @description Axios interceptor adalah sebuah fitur yang memungkinkan kita 
+   * untuk melakukan manipulasi data sebelum atau sesudah melakukan request dengan Axios. 
+   * Contohnya adalah menambahkan header pada setiap request.
+   */
+  apiClient.interceptors.request.use((config) => {
+    // Mengambil token dari cookie
+    const token = cookieHandler.get("token");
+    // Jika token ada, tambahkan header authorization
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  });
 
   return apiClient;
 }
-
-
-
