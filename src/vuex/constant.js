6b1d0568cd622b0f8ce261a042d8de470fdda5f4
yeremiadio <raikkonendio@gmail.com>
2023-02-14 12:01:50 +0700

    refactor constant

--- src/vuex/constant.js
+++ src/vuex/constant.js
@@ -1,8 +1,26 @@
+export const SET_IS_LOADING = "SET_IS_LOADING";
+/**
+ * @description Constants for Task (Penugasan)
+ */
 export const SET_TASK = "SET_TASK";
 export const DELETE_TASK = "DELETE_TASK";
+/**
+ * @description Constants for Report (Pelaporan)
+ */
 export const GET_ALL_REPORT = "GET_ALL_REPORT";
 export const CREATE_REPORT = "CREATE_REPORT";
 export const SET_REPORT = "SET_REPORT";
 export const UPDATE_REPORT = "UPDATE_REPORT";
 export const UPDATE_REPORTS = "UPDATE_REPORTS";
 export const DELETE_REPORT = "DELETE_REPORT";
+/**
+ * @description Constants for User (Pengguna)
+ */
+export const GET_USER = "GET_USER";
+export const SET_USER = "SET_USER";
+export const GET_ALL_USER = "GET_ALL_USER";
+export const DELETE_USER = "DELETE_USER";
+export const LOGOUT_USER = "LOGOUT_USER";
+export const UPDATE_USER = "UPDATE_USER";
+export const UPDATE_USERS = "UPDATE_USERS";
+export const CREATE_USER = "CREATE_USER";
